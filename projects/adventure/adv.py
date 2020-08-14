d8e54656419ff405dec3cd311b6cfbd55529fa95
Michael Perez <mperez6473@gmail.com>
2020-08-14 09:16:12 -0500

    have the dictionary setup. Trying to keep track of previous rooms in the current rooms information

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -47,75 +47,48 @@ player.current_room.print_room_description(player)
 
 traversal_path.append(player.current_room.id)
 
+print('#-' * 30)
 
-# def moving_rooms(current_room, direction):
-#     player.travel(direction)
-#     traversal_path.append(current_room.id)
-#     if current_room.id not in visited_rooms:
-#         visited_rooms.add(current_room.id)
-#     print(current_room)
+information = dict()
 
 
-# def visit_all_rooms(current_room):
-#     print('#########'*10)
-#     print(current_room)
-#     print(current_room.get_exits())
-#     s = Stack()
-#     s.push(current_room.get_exits())
-#     while s.size() > 0:
-#         v = s.pop()
-#         print(v)
-#         moving_rooms(player.current_room.id, v[0])
+def find_all_rooms(players_room):
+    s = Stack()
+    s.push(players_room)
+    while s.size() > 0:
+        v = s.pop()
+        # print(v)
+        # print(v.get_exits())
+        if player.current_room.id not in visited_rooms:
+            visited_rooms.add(player.current_room.id)
+        traversal_path.append(player.current_room.id)
+
+        for directions in v.get_exits():
+            # print(directions)
+            if players_room.id not in information:
+                information[players_room.id] = dict()
+            if directions not in information[players_room.id]:
+                information[players_room.id][directions] = '?'
+        # print(information[players_room.id])
+
+        for real_directions in information[players_room.id]:
+            print(information[players_room.id][real_directions])
+            if information[players_room.id][real_directions] == '?':
+                print(real_directions)
+                player.travel(real_directions)
+                print(player.current_room)
+                s.push(player.current_room)
+                # player.travel(real_directions)
+
+        # for d in v.get_exits():
 
 
-# visit_all_rooms(player.current_room)
-print('#' * 40)
-
-
-s = Stack()
-
-
-# def visit_all_rooms(players_room):
-#     s.push(players_room.get_exits()[0])
-#     print(players_room.get_coords())
-#     print(f'players_room: {players_room}')
-#     v = s.pop()
-#     print(v)
-#     for adjacent_rooms in players_room.get_exits():
-#         print(adjacent_rooms)
-#         player.travel(adjacent_rooms)
-#         traversal_path.append(player.current_room.id)
-#         if player.current_room.id not in visited_rooms:
-#             visited_rooms.add(player.current_room.id)
-#             visit_all_rooms(player.current_room)
-
-#     player.travel(v[0])
-
-#     if player.current_room.id not in visited_rooms:
-#         visited_rooms.add(players_room.id)
-#     traversal_path.append(players_room.id)
-#     for adjacent_rooms in players_room.get_exits():
-#         return visit_all_rooms(adjacent_rooms, s)
-
-
-# def visit_all_rooms(players_room):
-#     print(players_room.get_exits())
-#     s.push(players_room.get_exits())
-#     v = s.pop()
-#     print(v)
-#     if player.current_room.id not in visited_rooms:
-#         visited_rooms.add(player.current_room.id)
-#         for direction in player.current_room.get_exits():
-#             player.travel(direction)
-#             visit_all_rooms(player.current_room)
-
-
-# visit_all_rooms(player.current_room)
-
+find_all_rooms(player.current_room)
+print(information)
 print(f'traversal_path {traversal_path}')
 print(f'visited rooms: {visited_rooms}')
 
-print('#' * 40)
+print('#-' * 30)
 print('')
 print('~~~TESTS~~~')
 if len(visited_rooms) == len(room_graph):
@@ -128,25 +101,25 @@ print(visited_rooms)
 print('~~~END OF TESTS~~~')
 
 
-# while True:
+while True:
 
-#     cmds = input("-> ").lower().split(" ")
-#     if cmds[0] in ["n", "s", "e", "w"]:
-#         player.travel(cmds[0], True)
-#         if player.current_room.id not in visited_rooms:
-#             visited_rooms.add(player.current_room.id)
-#         traversal_path.append(player.current_room.id)
-#         print(f'traversal_path {traversal_path}')
-#         print(f'visited rooms: {visited_rooms}')
-#         # print('heeeeeeeeeyyyyyyy')
+    cmds = input("-> ").lower().split(" ")
+    if cmds[0] in ["n", "s", "e", "w"]:
+        player.travel(cmds[0], True)
+        if player.current_room.id not in visited_rooms:
+            visited_rooms.add(player.current_room.id)
+        traversal_path.append(player.current_room.id)
+        print(f'traversal_path {traversal_path}')
+        print(f'visited rooms: {visited_rooms}')
+        # print('heeeeeeeeeyyyyyyy')
 
-#         # print("possible directions")
+        # print("possible directions")
 
-#         for d in player.current_room.get_exits():
-#             print(d)
-#             print(player.current_room.get_room_in_direction(d).id)
+        for d in player.current_room.get_exits():
+            print(d)
+            print(player.current_room.get_room_in_direction(d).id)
 
-#     elif cmds[0] == "q":
-#         break
-#     else:
-#         print("I did not understand that command.")
+    elif cmds[0] == "q":
+        break
+    else:
+        print("I did not understand that command.")
